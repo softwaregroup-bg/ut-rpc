a4c45ebce28900888f1aa929fb01ce677639e9b9
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2015-12-02 11:34:23 +0200

    feat: add linting through npm test and ut-tools/ut-lint

--- index.js
+++ index.js
@@ -2,83 +2,77 @@ var rpc = require('./rpc-stream');
 var MuxDemux = require('mux-demux');
 
 function server(methods, isServer) {
-    var rpcs = rpc(methods,{id:isServer?'bus':'port'});
+    var rpcs = rpc(methods, {id: isServer ? 'bus' : 'port'});
     var mdm = MuxDemux({error: true});
 
     mdm.createLocalCall = rpcs.createLocalCall.bind(rpcs);
-    mdm.createRemote = function createRemote(name, type){
-        if(type == 'req') {
+    mdm.createRemote = function createRemote(name, type) {
+        if (type === 'req') {
             return rpcs.createRemoteCall(name);
-        } else if(type == 'pub') {
-            return rpcs.createRemoteCall(name);  //todo handle pub/sub
+        } else if (type === 'pub') {
+            return rpcs.createRemoteCall(name);  // todo handle pub/sub
         } else {
             return createRemoteStream(name, type);
         }
-    }
+    };
 
     function genManifest(methods) {
         var manifest = {};
-        for(var name in methods) {
+        for (var name in methods) {
             manifest[name] = methods[name]._rpcType || 'async';
         }
         return manifest;
     }
 
     mdm.on('connection', function(con) {
-        con.on('error', function(e) {console.log(e)});
+        con.on('error', function(e) { console.log(e); });
 
-        if(con.meta == 'rpc') {
-            if (isServer){
+        if (con.meta === 'rpc') {
+            if (isServer) {
                 con.pipe(rpcs).pipe(con);
             }
             return;
         }
 
-        if(con.meta == 'meta') {
+        if (con.meta === 'meta') {
             con.write(genManifest(methods));
             return;
         }
 
         try {
             var stream = methods[con.meta[0]](con.meta.slice(1));
-            if(stream.readable) {
+            if (stream.readable) {
                 stream.pipe(con);
             }
-            if(stream.writable) {
+            if (stream.writable) {
                 con.pipe(stream);
             }
-        } catch(err) {
+        } catch (err) {
             mdm.emit('error', err);
         }
     });
 
     var rpcStream = mdm.createStream('rpc');
-    rpcStream.on('error', function(e) {console.log(e)});
+    rpcStream.on('error', function(e) { console.log(e); });
     if (!isServer) {
         rpcs.pipe(rpcStream).pipe(rpcs);
     }
     var metaStream = mdm.createStream('meta');
 
     function createRemoteStream(name, type) {
-
         return function() {
             var args = [].slice.call(arguments);
 
-            var cb = ('function' == typeof args[args.length - 1])
-                ? args.pop()
-                : null;
+            (typeof args[args.length - 1] === 'function') && args.pop();
 
             var streamArgs = [name].concat(args);
 
             var newStream = (
-                type === 'readable'
-                    ? mdm.createReadStream(streamArgs)
-                    : type == 'writable'
-                    ? mdm.createWriteStream(streamArgs)
-                    : type == 'duplex'
-                    ? mdm.createStream(streamArgs)
-                    : (function () { throw new Error("unknown stream type: " + type) })()
-            )
+                type === 'readable' ? mdm.createReadStream(streamArgs)
+                    : type === 'writable' ? mdm.createWriteStream(streamArgs)
+                    : type === 'duplex' ? mdm.createStream(streamArgs)
+                    : (function() { throw new Error('unknown stream type: ' + type); })()
+            );
             newStream.autoDestroy = false;
 
             return newStream;
@@ -87,16 +81,15 @@ function server(methods, isServer) {
 
     metaStream.on('data', function(manifest) {
         var methods = {};
-        for(var name in manifest) {
-            if(manifest[name] == 'async') {
+        for (var name in manifest) {
+            if (manifest[name] === 'async') {
                 methods[name] = rpcs.createRemoteCall(name);
             } else {
                 methods[name] = createRemoteStream(name, manifest[name]);
             }
         }
         mdm.emit('remote', methods);
-    }.bind(mdm));
-
+    });
 
     return mdm;
 }
